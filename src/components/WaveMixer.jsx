07d99b450d84f7b9bd1c2f8945238384707b45ac
m-hatch <hatch@hq.acm.org>
2018-06-29 12:20:07 -0400

    clean up WaveMixer

--- src/components/WaveMixer.jsx
+++ src/components/WaveMixer.jsx
@@ -11,89 +11,27 @@ class WaveMixer extends React.Component {
 
     // file store
     this.data = [];
-
-    this.resetPlayhead = this.resetPlayhead.bind(this);
-    this.getFilenames = this.getFilenames.bind(this);
-    this.handleMenuChange = this.handleMenuChange.bind(this);
-    this.updateProgress = this.updateProgress.bind(this);
-    this.removeFile = this.removeFile.bind(this);
-    this.setFinished = this.setFinished.bind(this);
-
-    this.state = {
-      isPlaying: false,
-      isAtBeginning: true,
-      progress: 0.0,
-      audioFiles: [],
-      selectedFileId: null,
-      selectedFileName: null,
-      options: [], 
-      files: [],
-      cycle: true
-    };
-  }
-
-  setFinished() {
-    this.setState({ isPlaying: false })
-  }
-
-  updateProgress(progress) {
-    this.setState({ progress, isAtBeginning: false })
-  }
-
-  resetPlayhead() {
-    this.setState({ isAtBeginning: true })
-  }
-
-  getFilenames() {
-    return this.state.audioFiles.map(file => file.name)
-  }
-
-  handleMenuChange(e, idx) {
-    const newFile = this.data.find(el => el.id === e.value);
-    const newAudioFiles = this.state.audioFiles;
-    newAudioFiles[idx] = newFile;
-    this.setState({ audioFiles: newAudioFiles})
-  }
-
-  removeFile(idx) {
-    this.setState({isPlaying: false})
-    const newAudioFiles = Array.from(this.state.audioFiles);
-    newAudioFiles.splice(idx, 1)
-    this.setState({ audioFiles: newAudioFiles })
-  }
-
-  getWaveforms() {
-    this.state.audioFiles.map((file, i) => {
-      return (
-        <Waveform
-          disabled={false}
-          key={i}
-          idx={i}
-          src={file.url}
-          name={file.name}
-          options={this.state.options}
-          handleMenuChange={this.handleMenuChange} 
-          isPlaying={this.state.isPlaying}
-          progress={this.state.progress}
-          updateProgress={this.updateProgress}
-          isAtBeginning={this.state.isAtBeginning} 
-          removeFile={this.removeFile}
-          setFinished={this.setFinished}
-          resetPlayhead={this.resetPlayhead}
-          cycle={this.state.cycle} />
-      )
-    })
   }
 
   render() {
     return (
       <div>
-        
         <Header />
+        <Controls data={ this.data } />
 
-        <Controls data={this.data} />
-
-        { this.getWaveforms() }
+        { 
+          this.props.audioFiles.map((file, i) => {
+            return (
+              <Waveform
+                key={i}
+                idx={i}
+                disabled={ false }
+                data={ this.data }
+                src={ file.url }
+                name={ file.name } />
+            )
+          })
+        }
 
       </div>
     );
@@ -101,14 +39,11 @@ class WaveMixer extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return {}
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {}
+  return {
+    audioFiles: state.app.audioFiles
+  }
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(WaveMixer);
